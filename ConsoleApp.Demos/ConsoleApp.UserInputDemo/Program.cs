3c804bd63b3674997da88beede9d5eacc549296b
Pooria Alijani <YOUR_GITHUB_EMAIL@example.com>
2026-05-09 20:54:38 -0400

    Add project files.

---
+++ ConsoleApp.Demos/ConsoleApp.UserInputDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿string name = string.Empty;
+int age = 0;
+
+Console.WriteLine("Please enter your name:");
+name = Console.ReadLine();
+
+Console.WriteLine("Please enter your age:");
+age = Convert.ToInt32(Console.ReadLine());
---
+++ ConsoleApp.Demos/ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿string name = "Pooria";
+Console.WriteLine(name);
+Console.WriteLine("Hello " + name); // string concatenation
+Console.WriteLine($"Hello {name}"); // string interpolation, available since C# 6.0, allows you to embed expressions directly in string literals
+Console.WriteLine("Hello {0}", name); // string formatting, available since C# 1.0, allows you to format strings using placeholders and arguments   
+
+int Age = 28;
+int OrAge = 29;
+float avgAge = ((OrAge + Age) / 2);
+Console.WriteLine($"I am {Age} or {OrAge} years old. So the avg is {avgAge}");
