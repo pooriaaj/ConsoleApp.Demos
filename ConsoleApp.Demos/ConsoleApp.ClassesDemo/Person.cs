8afb007986cc204d6344a7014ad5c669af454acf
Pooria Alijani <YOUR_GITHUB_EMAIL@example.com>
2026-05-28 20:25:37 -0400

    Practiced Classes and Objects.

---
+++ ConsoleApp.Demos/ConsoleApp.ClassesDemo/Person.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+
+public class Person
+{
+    // Properties / Data Members
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+
+    // Field Members
+    private string _taxNumber;
+
+    // Method
+    public void PrintFullName()
+    {
+        Console.WriteLine($"Hello, my fisr name is {FirstName}, my last name is {LastName} and I am {DateOfBirth} years old.");
+    }
+    public void PrintInitials()
+    {
+        var firstInitial = FirstName[0];
+        var secondInitial = LastName[0];
+        Console.WriteLine($"My initials are {FirstName[0]}.{LastName[0]}.");
+    }
+
+    public void GenerateTaxNumber()
+    {
+        _taxNumber = RandomNumberGenerator.GetInt32(100000000, 999999999).ToString();
+    }
+
+    public string GetTaxNumber()
+    {
+        return _taxNumber;
+    }
+
+    public 
+}
---
+++ ConsoleApp.Demos/ConsoleApp.ClassesDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("*** Classes and Objects ***");
+Person person;
+
+Person baby = new Person();
+
+Console.WriteLine("Enter your first name: ");
+baby.FirstName = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine("Enter your last name: ");
+baby.LastName = Console.ReadLine() ?? string.Empty;
+
+Console.WriteLine("Enter your date of birth (yyyy-mm-dd): ");
+baby.DateOfBirth = Console.ReadLine() is string dobInput && DateOnly.TryParse(dobInput, out DateOnly dob) ? dob : default;
+
+
+baby.PrintFullName();
+baby.PrintInitials();
+baby.GenerateTaxNumber();
+
+var taxNumber = baby.GetTaxNumber();
+
+Console.WriteLine("Your tax number is: " + taxNumber);
